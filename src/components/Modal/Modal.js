9652bda3b1dca53bc7e6ec2270d8c834245f9dba
Scott <swschipke@gmail.com>
2019-12-07 15:01:06 -0700

    Add files for modal

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Route, NavLink, Link} from 'react-router-dom';
+import ReactModal from 'react-modal';
+import './Modal.scss';
+
+export const Modal = () => {
+  return (
+    
+  )
+}
