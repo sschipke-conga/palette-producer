d7e87f2d676f481b16820487feb57897d81fc4d7
posi7790 <posi7790@colorado.edu>
2019-12-07 17:16:09 -0700

    Create palette cards for to populate space in container

---
+++ src/components/PaletteCard/PaletteCard.js
@@ -0,0 +1,28 @@
+import './PaletteCard.scss';
+import React from 'react';
+import ColorPicker from '../ColorPicker/ColorPicker'
+import PropTypes from 'prop-types'
+import { IoIosLock, IoIosUnlock } from 'react-icons/io';
+
+export const PaletteCard = ({ color, changeColor, isLocked }) => {
+
+  const colorStyles = {
+    background: color,
+  }
+
+  return (
+    <div className='PaletteCard'>
+      <div className='color' style={colorStyles}>
+        {isLocked ? <IoIosLock /> : <IoIosUnlock />}
+        <p className='hex'>{color}</p>
+      </div>
+      <ColorPicker color={color} />
+    </div >
+  )
+}
+
+export default PaletteCard;
+
+PaletteCard.propTypes = {
+
+}
