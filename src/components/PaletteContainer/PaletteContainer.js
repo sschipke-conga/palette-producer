633e412035dbde1958e465d3e12d4e4669aba74c
posi7790 <posi7790@colorado.edu>
2019-12-07 17:16:09 -0700

    Create palette cards for to populate space in container

--- src/components/PaletteContainer/PaletteContainer.js
+++ src/components/PaletteContainer/PaletteContainer.js
@@ -1,58 +1,92 @@
 import React, { Component } from 'react';
-import './PaletteContainer.test';
+import './PaletteContainer.scss';
 import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { IoMdSave } from "react-icons/io";
-
+import PaletteCard from '../PaletteCard/PaletteCard';
 
 export class PaletteContainer extends Component {
   constructor() {
     super();
     this.state = {
-      currentProject= {
-        projectName: '',
-        paletteName: '',
-        colors: {
-          color1: '',
-          color2: '',
-          color3: '',
-          color4: '',
-          color5: '',
-        }
-      }
+      projectName: '',
+      paletteName: '',
+      color1: '',
+      color2: '',
+      color3: '',
+      color4: '',
+      color5: ''
     }
   }
 
+  componentDidMount = () => {
+    this.randomizePalette();
+  }
+
   handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  changeColor = (colorN, hexCode) => {
+    this.setState({ [colorN]: hexCode })
+  }
+
+  generateRandomHex = () => {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  randomizePalette = () => {
+    this.setState({
+      color1: this.generateRandomHex(),
+      color2: this.generateRandomHex(),
+      color3: this.generateRandomHex(),
+      color4: this.generateRandomHex(),
+      color5: this.generateRandomHex()
+    })
+  }
+
+  displayPalettes = colors => {
+    const colorKeys = Object.keys(colors);
+    return colorKeys.map(colorKey => {
+      return (
+        <PaletteCard
+          color={colors[colorKey]}
+          changeColor={this.changeColor}
+          isLocked={false}
+        />
+      )
+    })
   }
 
   render() {
     return (
       <main className='PaletteContainer'>
-        <header className='PalleteContainer-header'>
-          <div>
+        <header className='PaleteContainer-header'>
+          <div className='PaleteContainer-names'>
             <h1
-              contenteditable="true"
+              contentEditable="true"
               className='project-name'
               name='projectName'
               value={this.state.projectName}
               onChange={this.handleChange}>
-              `${this.state.projectName}`
+              {this.state.projectName === '' ? 'Enter project name' : this.state.projectName}
             </h1>
             <h2
-              contenteditable="true"
+              contentEditable="true"
               className='palette-name'
               name='paletteName'
               value={this.state.paletteName}
               onChange={this.handleChange}>
-              `${this.state.paletteName}`
+              {this.state.paletteName === '' ? 'Enter palette name' : this.state.paletteName}
             </h2>
           </div>
-          <div>
-            <IoMdSave />
+          <div className='icon-container'>
+            <IoMdSave className='save-icon' />
           </div>
         </header>
+        <section className='PaletteContainer-section'>
+          {this.displayPalettes([this.state.color1, this.state.color2, this.state.color3, this.state.color4, this.state.color5])}
+        </section>
       </main>
     )
   }
@@ -61,7 +95,7 @@ export class PaletteContainer extends Component {
 
 export default PaletteContainer;
 
-PalletteContainer.propTypes = {
+PaletteContainer.propTypes = {
 
 }
 
