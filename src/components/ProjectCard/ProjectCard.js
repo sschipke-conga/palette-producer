449cd385e47bc1bcd11015e997d8e4e77e34df41
posi7790 <posi7790@colorado.edu>
2019-12-10 15:10:42 -0700

    Add container to hol users projects and palettes

---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,34 @@
+import './ProjectCard.scss';
+import React from 'react';
+import PaletteCard from '../PaletteCard/PaletteCard';
+import PropTypes from 'prop-types';
+import { IoIosAddCircleOutline } from 'react-icons/io';
+
+export const ProjectCard = ({ palettes }) => {
+
+  console.log(palettes)
+
+  const displayPalettes = palettes.map((palette, index) => {
+    return (
+      <PaletteCard
+        key={'PaletteCard' + index}
+        colors={[palette.color1, palette.color2, palette.color3, palette.color4, palette.color5]}
+      />
+    )
+  })
+
+  return (
+    <div className='ProjectCard'>
+      <div>
+        <IoIosAddCircleOutline className='add-palette-icon' />
+      </div>
+      {displayPalettes}
+    </div >
+  )
+}
+
+export default ProjectCard;
+
+ProjectCard.propTypes = {
+
+}
