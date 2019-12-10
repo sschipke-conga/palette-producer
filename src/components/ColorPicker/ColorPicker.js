355865ba8b54d1dd25fb0bf59d02b14a51f06126
posi7790 <posi7790@colorado.edu>
2019-12-10 11:56:21 -0700

    Add ability to change color and lock colors

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ColorPicker.scss'
 import { CustomPicker } from 'react-color';
-const { Hue, Alpha, Saturation } = require('react-color/lib/components/common');
+const { Hue, Saturation } = require('react-color/lib/components/common');
 
 class ColorPicker extends React.Component {
   render() {
@@ -10,17 +10,12 @@ class ColorPicker extends React.Component {
         <div className='slider-container'>
           <Hue
             {...this.props}
-            onChange={this.handleChange} />
+            onChange={this.props.changeColor} />
         </div>
         <div className='slider-container'>
           <Saturation
             {...this.props}
-            onChange={this.handleChange} />
-        </div>
-        <div className='slider-container'>
-          <Alpha
-            {...this.props}
-            onChange={this.handleChange} />
+            onChange={this.props.changeColor} />
         </div>
       </div>
     );
