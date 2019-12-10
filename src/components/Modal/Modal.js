204a29ee926a5ad5105a7d669a728a6f30a70aae
Scott <swschipke@gmail.com>
2019-12-09 20:48:56 -0700

    Have Modal render SignupForm or LoginForm

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import {Route, NavLink, Link} from 'react-router-dom';
 import ReactModal from 'react-modal';
+import LoginForm from '../LoginForm/LoginForm';
+import SignupForm from '../SignupForm/SignupForm';
 import './Modal.scss';
 
 export const Modal = () => {
   return (
-    
+    <>
+      <Link to='/'>
+        <div className="modal-background">
+          <div className='modal-backdrop'></div>
+        </div>
+      </Link>
+      <div className='modal-div'>
+        <ul className='tabs'>
+          <NavLink to='/login' className='modal-link'>
+            <li className='modal-tab'>Log in</li>
+          </NavLink>
+          <NavLink to='/signup' className='modal-link'>
+            <li className='modal-tab'>Sign up</li>
+          </NavLink>
+        </ul>
+        <Route exact path='/login' render={() => <LoginForm />}
+        />
+        <Route exact path='/signup' render={() => <SignupForm />}
+        />
+      </div>
+    </>
   )
 }
+
+export default Modal;
