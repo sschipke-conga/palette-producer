f538f266ee0e8d8b6cf3a3fa92d82d179aea2915
posi7790 <posi7790@colorado.edu>
2019-12-10 15:44:32 -0700

    Add method to get projects and palettes for user when they log in

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,18 +11,27 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      userID: 2,
+      userID: 3,
       projects: [],
       palettes: {}
     }
   }
 
-  componentDidMount = async () => {
-    let projects = await getUserProjects(this.state.userID);
+  loadUserProjectsAndPalettes = async userID => {
+    this.setState(userID)
+    let projects = await getUserProjects(userID);
     this.setState({ projects });
     let palettes = await projects.reduce(async (acc, project) => {
       return acc[project.id] = await getProjectPalettes(project.id)
     }, {});
+    palettes = palettes.reduce((acc, palette) => {
+      if (!acc[palette.project_id]) {
+        acc[palette.project_id] = [palette];
+      } else {
+        acc[palette.project_id].push(palette)
+      }
+      return acc;
+    }, {})
     this.setState({ palettes })
   }
 
@@ -30,7 +39,7 @@ class App extends Component {
     return (
       <main className='App'>
         <Route exact path='/' render={() => <PaletteContainer />} />
-        {/* <Route exact path='/' render={() => <ProjectContainer projects={this.state.projects} palettes={this.state.palettes} />} /> */}
+        <Route exact path='/' render={() => <ProjectContainer projects={this.state.projects} palettes={this.state.palettes} />} />
       </main>
     )
   }
