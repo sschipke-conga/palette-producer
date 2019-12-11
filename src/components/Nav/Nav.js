17c57d3a190a4e00504310c1239eda2d6a749fb1
posi7790 <posi7790@colorado.edu>
2019-12-10 19:09:40 -0700

    Add logo

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Nav.scss'
+import logo from '../../assets/2026954.svg';
 
 const Nav = () =>
   <nav>
-    <img className="logo" src="../../assets/2026954.svg" alt="logo" />
+    <img className="logo" src={logo} alt="logo" />
     <NavLink exact to="/signup">Account</NavLink>
   </nav>
 
