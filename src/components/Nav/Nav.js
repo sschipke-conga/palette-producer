c5adf0c4dcfeed936a51c9c41ef2d0cb56e32f43
posi7790 <posi7790@colorado.edu>
2019-12-10 19:01:55 -0700

    Style nav and account modal

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
-const Nav = () => 
-<nav>
-  <img className="logo" src="" alt="logo"/>
-  <NavLink exact to="/signup">Account</NavLink>
-</nav>
+import { NavLink } from 'react-router-dom';
+import './Nav.scss'
+
+const Nav = () =>
+  <nav>
+    <img className="logo" src="../../assets/2026954.svg" alt="logo" />
+    <NavLink exact to="/signup">Account</NavLink>
+  </nav>
 
 export default Nav;
