ea30c47c72054740cb9d7f93021ef5a0619b8645
Scott <swschipke@gmail.com>
2019-12-21 17:04:37 -0700

    Fix hex code generation

--- src/components/PaletteContainer/PaletteContainer.js
+++ src/components/PaletteContainer/PaletteContainer.js
@@ -88,7 +88,11 @@ export class PaletteContainer extends Component {
   }
 
   generateRandomHex = () => {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    let hexCode = '#' + Math.floor(Math.random() * 16777215).toString(16);
+      if(hexCode.length < 7 ) {
+        return this.generateRandomHex()
+      }
+    return hexCode;
   }
 
   randomizePalette = () => {
