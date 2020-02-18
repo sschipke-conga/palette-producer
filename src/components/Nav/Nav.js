c2bebcd643bccfe2964218ba102e2d6040591016
Scott <swschipke@gmail.com>
2020-02-17 19:18:36 -0700

    Add logo to nav bar and favicon

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Nav.scss'
-import logo from '../../assets/2026954.svg';
+import logo from '../../assets/logo.svg';
 
 const Nav = () =>
   <nav>
