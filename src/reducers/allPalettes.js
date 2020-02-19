9567673650ffd830f72c200cb1f3793066af20be
Scott <swschipke@gmail.com>
2020-02-18 19:37:12 -0700

    Add project and palette info to the store

--- src/reducers/allPalettes.js
+++ src/reducers/allPalettes.js
@@ -1,10 +1,12 @@
 const allPalettes = (state = [], action) => {
   switch (action.type) {
-    case "SET_PALETTES":
-      return [...state, ...action.allPalettes];
-      case 'REMOVE_PALETTE':
-        let updatedPalettes = state.filter(palette => palette.id !== action.id)
-        return [...updatedPalettes]
+    case 'SET_PALETTES':
+      return [...action.allPalettes];
+    case 'REMOVE_PALETTE':
+      let updatedPalettes = state.filter(palette => palette.id !== action.id)
+      return [...updatedPalettes]
+    case 'ADD_PALETTE':
+      return [...state, action.palette]  
     default:
       return state;
   }
