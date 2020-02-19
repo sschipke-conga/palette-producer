f74e330f1c507652f18b2428e60814e4c8f64283
Scott <swschipke@gmail.com>
2020-02-18 21:13:15 -0700

    Flatten allPalettes to a single array

--- src/reducers/allPalettes.js
+++ src/reducers/allPalettes.js
@@ -1,12 +1,16 @@
 const allPalettes = (state = [], action) => {
   switch (action.type) {
     case 'SET_PALETTES':
-      return [...action.allPalettes];
+      return action.allPalettes.reduce((flatPalettes, groupOfPallets) =>{
+            groupOfPallets.forEach(palette => flatPalettes.push(palette))
+            return flatPalettes
+          }, [])
     case 'REMOVE_PALETTE':
       let updatedPalettes = state.filter(palette => palette.id !== action.id)
       return [...updatedPalettes]
     case 'ADD_PALETTE':
-      return [...state, action.palette]  
+      console.log('in reducer',action.palette)
+      return  [...state, action.palette]
     default:
       return state;
   }
